086f3aa284d829574a9774997982c5e9ffeaca32
Joshua Mun <joshmun23@gmail.com>
2015-10-13 10:18:33 -0400

    added semantic ui, all exporting is done client side, beginning to add publish/subscribe model to limit fields passed through documents

--- client/main.js
+++ client/main.js
@@ -2,28 +2,56 @@ Template.registerHelper("formatDate", function(date) {
   return moment.utc(date).format("DD/MM/YYYY");
 });
 
+Template.posts.onRendered(function(){
+  // initialize queries
+  Session.setDefault('postQueries', {});
+  var queries = Session.get('postQueries');
+
+  Meteor.call('setPosts', queries, function(err, success) {
+    if(err) {
+      $('#errors').append('<li>Could not retrieve Posts</li>')
+        .fadeIn()
+        .delay(1500)
+        .fadeOut(400, function(){
+          $(this).empty()
+        });
+
+    }
+    else {
+      Session.set('allPosts', success)
+    }
+  })
+});
+
 Template.posts.helpers({
   posts: function() {
-    return Posts.find({}, {category: 1});
+    // fetch queried posts for client
+    return Session.get('allPosts')
   }
 });
 
-// Template.post.helpers({
-//   post: function() {
-//     return Posts.findOne(Router.current().params._id);
-//   }
-// });
 
 Template.exportData.events({
   "click #exportData": function() {
-    alert('trigger');
-    debugger;
-    MyAppExporter.exportAllPosts();
+    // export current queried posts on page
+    MyAppExporter.exportAllPosts({
+      posts: Session.get('allPosts')
+    });
   }
 });
 
-// Template.post.events({
-//   "click #export": function() {
-//     MyAppExporter.exportContact(Router.current().params._id);
-//   }
-// });
+Template.filterPosts.helpers({
+  'categoriesList': function() {
+    // fetch a list of all field names from Posts for dropdown
+    // need to stringify the field names
+    var categories = Object.getOwnPropertyNames(Posts.findOne());
+    return categories
+  }
+})
+
+Template.filterPosts.onRendered(function () {
+  // add code for Semantic UI dropdowns
+  $('.ui.dropdown')
+    .dropdown()
+  ;
+});
